ddd0f0f51756a8c5312638d41d8a6ade9b9e3655
JPMarichal <jpmarichal@gmail.com>
2025-03-12 11:27:52 -0600

    Mejoras a los íconos de navegación.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Font Awesome -->
-    <link href="{{ asset('node_modules/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Livewire Styles -->
     @livewireStyles
--- resources/views/livewire/escrituras/vista-capitulo.blade.php
+++ resources/views/livewire/escrituras/vista-capitulo.blade.php
@@ -5,7 +5,7 @@
             <a href="/escrituras/{{ strtolower($capituloAnterior['libro']) }}/{{ $capituloAnterior['capitulo'] }}"
                class="flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-r-lg bg-white/95 shadow-md hover:bg-white hover:shadow-lg transition-all group"
                title="{{ $capituloAnterior['libro'] }} {{ $capituloAnterior['capitulo'] }}">
-                <i class="fas fa-chevron-left text-xl md:text-2xl text-gray-400 group-hover:text-gray-800"></i>
+                <i class="fa-solid fa-chevron-left text-xl md:text-2xl text-gray-400 group-hover:text-gray-800"></i>
             </a>
         @endif
     </div>
@@ -15,7 +15,7 @@
             <a href="/escrituras/{{ strtolower($capituloSiguiente['libro']) }}/{{ $capituloSiguiente['capitulo'] }}"
                class="flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-l-lg bg-white/95 shadow-md hover:bg-white hover:shadow-lg transition-all group"
                title="{{ $capituloSiguiente['libro'] }} {{ $capituloSiguiente['capitulo'] }}">
-                <i class="fas fa-chevron-right text-xl md:text-2xl text-gray-400 group-hover:text-gray-800"></i>
+                <i class="fa-solid fa-chevron-right text-xl md:text-2xl text-gray-400 group-hover:text-gray-800"></i>
             </a>
         @endif
     </div>
@@ -27,7 +27,7 @@
 
     @if($error)
         <div class="text-center text-red-600 mb-8 font-['Verdana']">
-            <i class="fas fa-exclamation-circle text-4xl mb-4"></i>
+            <i class="fa-solid fa-exclamation-circle text-4xl mb-4"></i>
             <p>{{ $error }}</p>
         </div>
     @endif
@@ -40,7 +40,7 @@
         </div>
     @elseif(!$error)
         <div class="text-center text-gray-600 font-['Verdana']">
-            <i class="fas fa-book-open text-4xl mb-4"></i>
+            <i class="fa-solid fa-book-open text-4xl mb-4"></i>
             <p>No se encontraron versículos para esta referencia.</p>
         </div>
     @endif
